7c3ba3a3e4b3706881254b6b0ac83c83d77d11f0
Bonsak Schieldrop <bonsak@racecar.no>
2015-05-26 20:19:26 +0200

    First commit

---
+++ source/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H__
+#define MAIN_H__
+
+#include "c4d_basedocument.h"
+//#include "painting/advanced/registeradvancedpaint.h"
+
+//Bool RegisterCustomDatatypeCustomGUI();
+
+void CommandLineRendering(C4DPL_CommandLineArgs* args);
+
+#endif // MAIN_H__
